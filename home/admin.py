619b64a494ec596b330753a5cb530d6d2bde8dfa
MrAalu <lulamit16@gmail.com>
2023-10-12 19:06:56 +0545

    Basic Product Models Created

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+    ProductCategory,
+    ProductBrand,
+    ProductModel,
+    OrderItemModel,
+    OrderModel,
+)
 
-# Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(ProductBrand)
+admin.site.register(ProductModel)
+admin.site.register(OrderItemModel)
+admin.site.register(OrderModel)
--- home/models.py
+++ home/models.py
@@ -2,30 +2,58 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-product_category_choices = {
-    "Exhauts",
-    "Stickers",
-    "Helmets",
-    "Jackets",
-    "Gloves",
-    "Masks",
-    "Pants",
-    "Boots",
-}
-# Not a VALID Choice
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
-# class ProductModel(models.Model):
-#     title = models.CharField(max_length=100)
-#     image = models.ImageField(upload_to="products/")
-#     original_price = models.DecimalField()
-#     sale_price = models.DecimalField()
-#     description = models.TextField()
-#     category = models.Choices(product_category_choices)
-#     brand = models.CharField()
-#     quantity = models.IntegerField()
-#     created_at = models.DateField(auto_now=True)
+class ProductBrand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
-# TODO : Category pani afai le create garna milne
-# Brand also
+class ProductModel(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="products/")
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# Users adding Products to cart creates new instance of this model
+class OrderItemModel(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+# when user goes to Payment, this instance is created
+ORDER_STATUS_CHOICES = [
+    ("Processing", "Processing"),
+    ("Shipping", "Shipping"),
+    ("Delivered", "Delivered"),
+    ("Cancelled", "Cancelled"),
+]
+
+
+class OrderModel(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_status = models.CharField(
+        max_length=50, choices=ORDER_STATUS_CHOICES, default="Processing"
+    )
+
+    # Get total Price , Sale price if exists then use that else original Price
