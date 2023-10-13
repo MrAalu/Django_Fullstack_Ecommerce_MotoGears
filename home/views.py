ecce1655b7df98ae551ff4184119ec423c615cf7
MrAalu <lulamit16@gmail.com>
2023-10-13 11:30:36 +0545

    Discount Products Functionality added

--- home/views.py
+++ home/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib import messages
+from .models import ProductModel
+from decimal import Decimal
 
 
 class Homepage(View):
     def get(self, request):
-        return render(request, "home/index.html")
+        # Fetch products with Discounts
+        discount_products = ProductModel.objects.filter(sale_price__isnull=False)
+
+        # Calculate discount percentage for each product
+        for product in discount_products:
+            discount_percentage = (
+                (product.original_price - product.sale_price) / product.original_price
+            ) * 100
+
+            # Decimal() to make Precise Decimal Conversion, Quantize() to make decimal places of 2
+            product.discount_percentage = Decimal(discount_percentage).quantize(
+                Decimal("0.00")
+            )
+
+        data = {"discount_products": discount_products}
+
+        return render(request, "home/index.html", data)
