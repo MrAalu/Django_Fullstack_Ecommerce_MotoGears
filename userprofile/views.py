a7b7b02c3c4e17e149d49906e015eefbc41f50fd
MrAalu <lulamit16@gmail.com>
2023-12-07 14:58:09 +0545

    Profile section added on UserProfile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from home.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, get_object_or_404
 
 
+@method_decorator(login_required(login_url="/accounts/login/"), name="dispatch")
 class ProfileView(View):
     def get(self, request):
         data = {}
         return render(request, "userprofile/dashboard.html", data)
 
 
+@method_decorator(login_required(login_url="/accounts/login/"), name="dispatch")
 class MyOrdersView(View):
     def get(self, request):
         excluded_statuses = ["Delivered", "Cancelled"]
@@ -27,6 +33,7 @@ class MyOrdersView(View):
         return render(request, "userprofile/myorders.html", data)
 
 
+@method_decorator(login_required(login_url="/accounts/login/"), name="dispatch")
 class OrderHistoryView(View):
     def get(self, request):
         excluded_statuses = ["Processing", "Shipping"]
@@ -46,6 +53,9 @@ class OrderHistoryView(View):
 
 
 # User's Profile Details
+@method_decorator(login_required(login_url="/accounts/login/"), name="dispatch")
 class UserProfileView(View):
     def get(self, request):
-        return render(request, "userprofile/profile.html")
+        user_data = get_object_or_404(User, pk=request.user.id)
+        data = {"user_data": user_data}
+        return render(request, "userprofile/profile.html", data)
