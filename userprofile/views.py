4ea2ea4a1d2e2a5faec08c54d96bf06a9d0f3321
MrAalu <lulamit16@gmail.com>
2023-12-07 14:31:15 +0545

    Order History added on UserProfile

--- userprofile/views.py
+++ userprofile/views.py
@@ -29,7 +29,20 @@ class MyOrdersView(View):
 
 class OrderHistoryView(View):
     def get(self, request):
-        return render(request, "userprofile/orderhistory.html")
+        excluded_statuses = ["Processing", "Shipping"]
+        myorders = (
+            OrderModel.objects.filter(customer_id=request.user)
+            .exclude(order_status__in=excluded_statuses)
+            .order_by("-order_date")
+        )
+
+        data = {"myorders": myorders}
+
+        for order in myorders:
+            purchased_items = PurchasedItemModel.objects.filter(order=order)
+            order.purchased_items = purchased_items
+
+        return render(request, "userprofile/orderhistory.html", data)
 
 
 # User's Profile Details
