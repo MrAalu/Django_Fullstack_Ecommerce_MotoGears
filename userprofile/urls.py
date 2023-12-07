da38583fabcb10ff38101867a893b6299c089cde
MrAalu <lulamit16@gmail.com>
2023-12-07 12:51:02 +0545

    Navbar cart uses SUP for Cart Counter

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", ProfileView.as_view(), name="profile"),
     path("myorders/", MyOrdersView.as_view(), name="myorders"),
     path("orderhistory/", OrderHistoryView.as_view(), name="orderhistory"),
-    path("userprofile", UserProfileView.as_view(), name="userprofile"),
+    path("userprofile/", UserProfileView.as_view(), name="userprofile"),
 ]
